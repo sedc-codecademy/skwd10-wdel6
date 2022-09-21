1cab0deff0e91c658b80d3026dd06001be04fdd1
Wekoslav Stefanovski <swekster@gmail.com>
2022-09-21 20:51:57 +0200

    Class two - our first server

---
+++ SedcServer10/FirstDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var address = IPAddress.Any;
+var port = 668; //the neighbour of the beast
+
+TcpListener listener = new TcpListener(address, port);
+listener.Start();
+
+Console.WriteLine("Starting server loop");
+while (true)
+{
+    // wait for a request
+    Console.WriteLine($"Waiting for tcp client");
+    var client = listener.AcceptTcpClient();
+    Console.WriteLine($"Accepted tcp client");
+    // process request and get response
+    using var stream = client.GetStream();
+    byte[] buffer = new byte[8192];
+    Span<byte> bytes = new Span<byte>(buffer);
+    var byteCount = stream.Read(bytes);
+    // send out response
+    Console.WriteLine($"Read out {byteCount} bytes");
+    var data = Encoding.UTF8.GetString(bytes);
+    Console.WriteLine(data);
+    var responseString = @$"HTTP/1.1 200 OK
+
+Hello from SEDC Server";
+    var responseBytes = Encoding.UTF8.GetBytes(responseString);
+    stream.Write(responseBytes);
+    client.Close();
+}
+
---
+++ SedcServer10/StringBuilderDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+var bytes = Enumerable.Range(1, 1_000_000);
+
+//Stopwatch sw = Stopwatch.StartNew();
+//string result = "";
+//foreach (var dataByte in bytes)
+//{
+//    result += dataByte.ToString() + ":";
+//}
+//sw.Stop();
+//Console.WriteLine(result.Length);
+//Console.WriteLine($"Elapsed {sw.ElapsedMilliseconds}ms");
+
+
+Stopwatch sw2 = Stopwatch.StartNew();
+StringBuilder sb = new StringBuilder();
+foreach (var dataByte in bytes)
+{
+    sb.Append(dataByte.ToString() + ":");
+}
+sw2.Stop();
+var result2 = sb.ToString();
+Console.WriteLine(result2.Length);
+Console.WriteLine($"Elapsed {sw2.ElapsedMilliseconds}ms");
+
+
